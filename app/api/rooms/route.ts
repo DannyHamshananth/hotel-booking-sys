9e15abb998875e7a2e68bd91e590091c78cbfbe4
Danny Hamshananth <hamshananth@gmail.com>
2025-11-02 18:15:50 +0530

    Initial API for Room Search

--- app/api/rooms/route.ts
+++ app/api/rooms/route.ts
@@ -2,6 +2,20 @@ import prisma from '@/lib/prisma'
 import { NextResponse, NextRequest } from "next/server"
 
 export async function GET(request: NextRequest) {
-  const rooms = await prisma.room.findMany();
+  const { searchParams } = new URL(request.url);
+
+  const day = searchParams.get('day')|| undefined;
+  const persons = parseInt(searchParams.get('persons') as string);
+
+  const rooms = await prisma.bookingDay.findMany({
+    where: {
+      day: day,
+      user_id: {equals:null},
+      room: {persons: {gte:persons}},
+    },
+    include: { 
+      room:true
+    },
+  });
   return NextResponse.json(rooms);
 }
