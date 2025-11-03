cccc43aab47f35ef8687ac72d76cce56b851d560
Danny Hamshananth <hamshananth@gmail.com>
2025-11-03 18:13:24 +0530

    Card component with dynamic data

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -1,23 +1,25 @@
 import './Card.css'
 
-export default function Card() {
+export default function Card({room}:any) {
+  const booking = (e:any) => {
+    console.log(e.target.id)
+  }
   return (
     <div className="card">
       <div className="card-left">
         <div className="image-placeholder">340 × 210</div>
         <div className="room-info">
-          <h3>ROOM 1 TITLE</h3>
-          <p className="subtitle">LOREM IPSUM DOLOR SIT AMET</p>
+          <h3>{room.room.title_1}</h3>
+          <p className="subtitle">{room.room.title_2}</p>
           <p className="desc">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ac ex a risus dapibus
-            pharetra facilisis ac felis.
+            {room.room.desc}
           </p>
         </div>
       </div>
       <div className="card-right">
-        <p className="price">S$1,080<span>/night</span></p>
+        <p className="price">${room.room.amount.toLocaleString()}<span>/night</span></p>
         <p className="note">Subject to GST and charges</p>
-        <button className="book-btn">BOOK ROOM</button>
+        <button className="book-btn" id={room.id} onClick={(e)=> {booking(e)}}>BOOK ROOM</button>
       </div>
     </div>
   );
