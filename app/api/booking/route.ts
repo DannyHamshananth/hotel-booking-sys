51ce7648d126c365f739f0b6600c20fb211a4b94
Danny Hamshananth <hamshananth@gmail.com>
2025-11-06 17:50:32 +0530

    Booking API

---
+++ app/api/booking/route.ts
@@ -0,0 +1,46 @@
+import prisma from "@/lib/prisma"
+import { NextResponse, NextRequest } from "next/server"
+
+import { getToken } from "next-auth/jwt";
+
+const secret = process.env.NEXTAUTH_SECRET;
+
+export async function POST(req: NextRequest) {
+  const token = await getToken({ req: req, secret });
+  const body = await req.json();
+  const { id, guests, room_charge, title, name, email } = body;
+  const user_id:any = token?.id;
+
+  console.log(user_id);
+  console.log(body);
+
+  try {
+    const booking = await prisma.bookingDay.update({
+      where: { id: id},
+      data: {
+        user_id,
+        guests,
+        bookingInfo: {
+          create: {
+            room_charge,
+            extra_charges: Number(room_charge*0.09),
+            title,
+            name,
+            email,
+          }
+        },
+      },
+      include: {
+        bookingInfo: true,
+        room: true
+      }
+    });
+
+    return NextResponse.json(booking, { status: 201 })
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json({ error: 'Failed to create booking' }, { status: 500 })
+  }
+
+}
+
