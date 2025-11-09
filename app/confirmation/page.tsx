26165ce81607119a2768b9ec7de83aa2254e9a9c
Danny Hamshananth <hamshananth@gmail.com>
2025-11-09 16:40:09 +0530

    Enclosed in suspense to avoid Vercel error

--- app/confirmation/page.tsx
+++ app/confirmation/page.tsx
@@ -3,6 +3,7 @@ import { useSearchParams, useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 import { format, addDays } from "date-fns";
 import { useSession } from "next-auth/react";
+import { Suspense } from 'react'
 import './confirmation.css'
 
 export default function BookingConfirmation() {
@@ -45,46 +46,48 @@ export default function BookingConfirmation() {
   if (!booking) return <p>Loading...</p>;
 
   return (
-    <div className="confirmation-container">
-      <h2 className="confirmation-title">YOUR BOOKING HAS BEEN CONFIRMED</h2>
-      <p className="confirmation-subtitle">
-        We have sent your booking confirmation to the email address that you have provided.
-      </p>
+    <Suspense>
+      <div className="confirmation-container">
+        <h2 className="confirmation-title">YOUR BOOKING HAS BEEN CONFIRMED</h2>
+        <p className="confirmation-subtitle">
+          We have sent your booking confirmation to the email address that you have provided.
+        </p>
 
-      <div className="booking-info">
-        <p>
-          <strong>Check-in/Check-out:</strong> {booking?.day ?format(booking.day, "MMM dd, yyyy"):""} – {booking?.day ?format(addDays(booking.day, 1), "MMM dd, yyyy"):""}
-        </p>
-        <p>
-          <strong>Booking Confirmation Number:</strong> RES{booking.id}
-        </p>
-        <p>
-          <strong>Total Price for 1 Night:</strong> {booking?.bookingInfo?.room_charge ?(Number(booking.bookingInfo.room_charge)+Number(booking.bookingInfo.extra_charges)).toLocaleString('en-SG', { style: 'currency', currency: 'SGD' }): ""}
-        </p>
-      </div>
+        <div className="booking-info">
+          <p>
+            <strong>Check-in/Check-out:</strong> {booking?.day ? format(booking.day, "MMM dd, yyyy") : ""} – {booking?.day ? format(addDays(booking.day, 1), "MMM dd, yyyy") : ""}
+          </p>
+          <p>
+            <strong>Booking Confirmation Number:</strong> RES{booking.id}
+          </p>
+          <p>
+            <strong>Total Price for 1 Night:</strong> {booking?.bookingInfo?.room_charge ? (Number(booking.bookingInfo.room_charge) + Number(booking.bookingInfo.extra_charges)).toLocaleString('en-SG', { style: 'currency', currency: 'SGD' }) : ""}
+          </p>
+        </div>
 
-      <div className="details-card">
-        <div className='left-card'>
-        <div className="room-section">
-          <div className="room-image"></div>
-          <div>
-            <h4>ROOM: {booking?.room?.title_1 ? booking.room.title_1: ""}</h4>
-            <p>{booking.guests} {booking.guests == 1 ? 'Guest' : 'Guests'}</p>
+        <div className="details-card">
+          <div className='left-card'>
+            <div className="room-section">
+              <div className="room-image"></div>
+              <div>
+                <h4>ROOM: {booking?.room?.title_1 ? booking.room.title_1 : ""}</h4>
+                <p>{booking.guests} {booking.guests == 1 ? 'Guest' : 'Guests'}</p>
+              </div>
+            </div>
+            <div className="price-list">
+              <p>Room <span>{booking?.bookingInfo?.room_charge ? (Number(booking.bookingInfo.room_charge)).toLocaleString('en-SG', { style: 'currency', currency: 'SGD' }) : ""}</span></p>
+              <p>Tax & Service Charges (9%) <span>{booking?.bookingInfo?.extra_charges ? (Number(booking.bookingInfo.extra_charges)).toLocaleString('en-SG', { style: 'currency', currency: 'SGD' }) : ""}</span></p>
+              <p className="total">Total Price <span>{booking?.bookingInfo?.room_charge && booking?.bookingInfo?.extra_charges ? (Number(booking.bookingInfo.room_charge) + Number(booking.bookingInfo.extra_charges)).toLocaleString('en-SG', { style: 'currency', currency: 'SGD' }) : ""}</span></p>
+            </div>
+          </div>
+
+          <div className="guest-section">
+            <h5>GUEST DETAILS</h5>
+            <p>Name: {booking?.bookingInfo?.title || ""} {booking?.bookingInfo?.name}</p>
+            <p>Email Address: {booking?.bookingInfo?.email || ""}</p>
           </div>
         </div>
-          <div className="price-list">
-            <p>Room <span>{booking?.bookingInfo?.room_charge ? (Number(booking.bookingInfo.room_charge)).toLocaleString('en-SG', { style: 'currency', currency: 'SGD' }): ""}</span></p>
-            <p>Tax & Service Charges (9%) <span>{booking?.bookingInfo?.extra_charges ? (Number(booking.bookingInfo.extra_charges)).toLocaleString('en-SG', { style: 'currency', currency: 'SGD' }): ""}</span></p>
-            <p className="total">Total Price <span>{booking?.bookingInfo?.room_charge && booking?.bookingInfo?.extra_charges ? (Number(booking.bookingInfo.room_charge)+Number(booking.bookingInfo.extra_charges)).toLocaleString('en-SG', { style: 'currency', currency: 'SGD' }): ""}</span></p>
-          </div>
-        </div>
-
-        <div className="guest-section">
-          <h5>GUEST DETAILS</h5>
-          <p>Name: {booking?.bookingInfo?.title || ""} {booking?.bookingInfo?.name}</p>
-          <p>Email Address: {booking?.bookingInfo?.email || ""}</p>
-        </div>
       </div>
-    </div>
+    </Suspense>
   );
 }
