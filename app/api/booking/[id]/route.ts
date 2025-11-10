e78af3f319d553db8b7a4f8c921c864afebd300a
Danny Hamshananth <hamshananth@gmail.com>
2025-11-10 13:34:54 +0530

    Delete API added

--- app/api/booking/[id]/route.ts
+++ app/api/booking/[id]/route.ts
@@ -2,7 +2,6 @@ import prisma from "@/lib/prisma"
 import { NextResponse, NextRequest } from "next/server"
 
 import { getToken } from "next-auth/jwt";
-import { equal } from "assert";
 const secret = process.env.NEXTAUTH_SECRET;
 
 export async function GET(req: NextRequest, { params }:any) {
@@ -28,5 +27,33 @@ export async function GET(req: NextRequest, { params }:any) {
     return NextResponse.json(booking)
   } catch (error) {
     return NextResponse.json({ error: 'Booking not found' }, { status: 400 })
+  }
+}
+
+export async function DELETE(req: NextRequest, { params }:any) {
+  const { id } = await params;
+  const token = await getToken({ req: req, secret });
+  const user_id = token?.id ;
+
+  try {
+
+    const booking = await prisma.bookingDay.findUnique({
+      where: { id: Number(id) }
+    });;
+
+    if (token?.id == booking?.user_id) {
+      await prisma.bookingInfo.delete({ where: { booking_slot_id: Number(id) } });
+      await prisma.bookingDay.delete({ where: { id: Number(id) } });
+      return NextResponse.json({ message: "Room and related bookings deleted" });
+    } else {
+      return NextResponse.json(
+        { error: "Unauthorized request" },
+        { status: 403 }
+      );
+    }
+
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json({ error: 'Error occoured!' }, { status: 400 })
   }
 }
